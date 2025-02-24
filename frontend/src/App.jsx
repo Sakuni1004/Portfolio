cf9a72a6ecd06143c922a37d138fb2c518d3f1e7
Sakuni1004 <sakunikakarunathilaka99@gmail.com>
2025-02-24 12:35:37 +0530

    create folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { React } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Portfolio from './pages/portfolio'
+
+function App() {
+ 
+
+  return (
+   <Div>App</Div>
+  )
+}
+
+export default App
